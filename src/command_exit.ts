170ae553a6fa1bd32396447d4721a326b612e703
ManoloEsS <manolo.estrada.s@gmail.com>
2026-02-24 20:05:42 -0700

    refactor REPL - Add state type

---
+++ src/command_exit.ts
@@ -0,0 +1,9 @@
+import { State } from "./state.js";
+
+export function commandExit(state: State) {
+    console.log(`Closing the Pokedex... Goodbye!`);
+
+    state.readline.close()
+    process.exit(0);
+};
+
--- src/repl.ts
+++ src/repl.ts
@@ -1,24 +1,30 @@
-import { createInterface } from "node:readline";
+import { State } from "./state.js";
 
-export function startREPL(): void {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "Pokedex >",
-    });
+export function startREPL(state: State): void {
+    state.readline.prompt();
 
-    rl.prompt();
-
-    rl.on("line", (line) => {
+    state.readline.on("line", async (line) => {
         const words = cleanInput(line);
         if (words.length === 0) {
-            rl.prompt();
+            state.readline.prompt();
             return;
         }
 
         const commandName = words[0];
-        console.log(`Your command was: ${commandName}`);
-        rl.prompt();
+        const cmd = state.commands[commandName];
+        if (!cmd) {
+            console.log("Unknown command");
+            return state.readline.prompt();
+        }
+
+        try {
+            cmd.callback(state);
+
+        } catch (e) {
+            console.error(e);
+        }
+
+        state.readline.prompt();
     });
 }
 
@@ -29,3 +35,5 @@ export function cleanInput(s: string): string[] {
         .split(" ")
         .filter((word: string) => word !== "");
 }
+
+
